dad6dc592195ebc1806be126b5a1047a9c70b2f2
Thibault <talw3g@protonmail.com>
2025-10-22 22:45:25 +0200

    WIP on position control

--- software/src/main.rs
+++ software/src/main.rs
@@ -7,6 +7,8 @@ use cortex_m_rt::entry;
 
 use stm32f0xx_hal::{adc, pac, prelude::*, pwm, serial};
 
+use heapless::spsc::Queue;
+
 use core::fmt::Write;
 
 use core::panic::PanicInfo;
@@ -16,9 +18,15 @@ fn panic(_pi: &PanicInfo) -> ! {
     // reboot in bootloader mode
     cortex_m::peripheral::SCB::sys_reset();
 }
+const AVG_RANGE: usize = 100; // number of values for avg
+const POS_MAX_MV: u16 = 2800; // mv reading when pot at 100%
+const POS_MAX_ERR: u16 = 10; // pos error (%) for full speed
+const POS_MIN_ERR: f32 = 0.1; // authorized pos error (%)
 
 #[entry]
 fn main() -> ! {
+    // -- CONFIG --
+
     let mut p = pac::Peripherals::take().unwrap();
     let mut _cp = cortex_m::peripheral::Peripherals::take().unwrap();
 
@@ -57,33 +65,64 @@ fn main() -> ! {
     let mut adc = adc::Adc::new(p.ADC, &mut rcc);
     let mut ctrl_pot_pin = cortex_m::interrupt::free(|cs| gpioa.pa0.into_analog(cs));
     let mut pos_pot_pin = cortex_m::interrupt::free(|cs| gpioa.pa1.into_analog(cs));
+    let mut _visen_pin = cortex_m::interrupt::free(|cs| gpioa.pa2.into_analog(cs));
 
     // -- PWMs --
     let pwm_pin = cortex_m::interrupt::free(|cs| gpioa.pa7.into_alternate_af1(cs));
     let mut pwm = pwm::tim3(p.TIM3, pwm_pin, &mut rcc, 10.khz());
-    //
-    // enable PWM output
     pwm.enable();
 
+    // -- QUEUES --
+    let mut ctrl_pot_queue: Queue<u16, AVG_RANGE> = Queue::new();
+    let mut pos_pot_queue: Queue<u16, AVG_RANGE> = Queue::new();
     loop {
         // toggle led
         led.toggle().ok();
-        hb_dir.toggle().ok();
 
         // measure voltage on ADC
         let ctrl_pot: u16 = adc.read_abs_mv(&mut ctrl_pot_pin);
+        ctrl_pot_queue.enqueue(ctrl_pot).ok();
         let pos_pot: u16 = adc.read_abs_mv(&mut pos_pot_pin);
-        let max_adc = adc.max_sample() as u32;
+        pos_pot_queue.enqueue(pos_pot).ok();
+
+        if !(ctrl_pot_queue.is_full() & pos_pot_queue.is_full()) {
+            continue;
+        }
 
         // output measured voltage to serial
-        write!(s, "ctrl/pos: {ctrl_pot}/{pos_pot}").ok();
+        let ctrl = mean(&ctrl_pot_queue);
+        let pos = mean(&pos_pot_queue);
+        let pos_err: f32 = (pos as f32 - ctrl as f32) / POS_MAX_MV as f32 * 100.0;
+        let speed: u16 = ((pos_err.abs() as u16 * 100) / POS_MAX_ERR).min(100);
+        // writeln!(s, "pos_err: {pos_err}").ok();
+        // writeln!(s, "speed: {speed}").ok();
 
-        // set PWM according to ADC
-        let max_pwm = pwm.get_max_duty() as u32;
-        let duty = ((ctrl_pot as u32) * max_pwm) / max_adc;
-        pwm.set_duty(duty as u16);
+        if pos_err.abs() <= POS_MIN_ERR {
+            pwm.set_duty(0);
+        } else {
+            if pos_err < 0.0 {
+                hb_dir.set_high().ok();
+            } else {
+                hb_dir.set_low().ok();
+            }
+            pwm.set_duty(speed);
+        }
 
+        let _ = ctrl_pot_queue.dequeue();
+        let _ = pos_pot_queue.dequeue();
         // wait some time
-        cortex_m::asm::delay(10_000_000);
+        cortex_m::asm::delay(100_000);
     }
+}
+
+fn sum(queue: &Queue<u16, AVG_RANGE>) -> u32 {
+    let mut s: u32 = 0;
+    for val in queue.iter() {
+        s += *val as u32;
+    }
+    s
+}
+
+fn mean(queue: &Queue<u16, AVG_RANGE>) -> u16 {
+    (sum(queue) / (queue.len() as u32)) as u16
 }
